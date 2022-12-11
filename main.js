a22d9689733e523b180615e204fd364fec7f5b08
Fausto764 <83379618+Fausto764@users.noreply.github.com>
2022-12-11 11:47:42 -0300

    Slider terminado
    EL slider y un poco del efecto scroll

--- main.js
+++ main.js
@@ -1,5 +1,59 @@
+//nav
+
 hamburguer  = document.querySelector('.hamburger');
 hamburguer.onclick = function(){
     navBar = document.querySelector('.nav-bar');
     navBar.classList.toggle('active');
 }
+
+
+
+
+//slider
+
+const slider = document.querySelector('#slider');
+let sliderSection = document.querySelectorAll('.slider-section');
+let sliderSectionLast = sliderSection[slider-Selection.length -1];
+
+const btnLeft = document.querySelector('#button-left');
+const btnRight = document.querySelector('#button-right');
+
+function next (){
+    let sliderSectionFirst = document.querySelectorAll('.slider-section')[0];
+    slider.style.marginLeft="-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout( ()=>{
+        slider.style.transition="none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    },500);
+
+}
+function prev(){
+    let sliderSection = document.querySelectorAll('.slider-section');
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft="0";
+    slider.style.transition = "all 0.5s";
+    setTimeout( ()=>{
+        slider.style.transition="none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    },500);
+}
+btnRight.addEventListener('click', next);
+btnLeft.addEventListener('click', prev);
+
+//scroll effect
+
+const altoPantalla = window.innerHeight;
+const anchoPantalla = window.innerWidth;
+// let scrollDown = getBoundingClientRect().top;
+let header = document.querySelector('header');
+let navTexts = document.querySelectorAll('.nav-texts');
+
+addEventListener('scroll', ()=>{
+    if (scroll != 0 && anchoPantalla > 800){
+        header.style.height="60px"
+        navTexts.style.fontSize="";
+    }
+})
