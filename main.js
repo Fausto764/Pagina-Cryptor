40a50719a5c10fcd80e6c3b1033fce05c4489946
Fast <83379618+Fausto764@users.noreply.github.com>
2023-11-23 22:48:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,11 @@
         navBar.classList.toggle('active');
     }
 
-
+//link whatsapp
+ function irAPagina2() {
+      // Cambiar la ubicación a la Página 2
+      window.location.href = ' https://w.app/nlsdPL';
+    }
 
 //slider 1 
 
